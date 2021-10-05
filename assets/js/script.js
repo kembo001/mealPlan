1b49308892db1d857f14dbbf774b02989e10b361
Basquiat <ememdaniel98@gmail.com>
2021-10-05 11:45:08 -0500

    added to js so that ingredients would populate in ingredients box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 var confirmButton = $(".cuisineConfirm")
 var cuisineSelection;
 
@@ -7,26 +6,26 @@ var cuisineSelection;
 function getAPI() {
     var url = "https://api.spoonacular.com/recipes/complexSearch?cuisine=" + cuisineSelection + "&apiKey=7422e4770d3c4bdbb9679e356fa65ecf";
     fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        displayResults(data);
-    })
-    
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            displayResults(data);
+        })
+
 }
 
-function displayResults (data) {
-    for( i = 0; i < 8; i++) {
-    var resultsContainer = $(".searchResults")
-    var recipeResults = `<button class="recipe" value=${data.results[i].id}> ${data.results[i].title}</button>`
-   
-    console.log(recipeResults)
-    resultsContainer.append(recipeResults)
+function displayResults(data) {
+    for (i = 0; i < 8; i++) {
+        var resultsContainer = $(".searchResults")
+        var recipeResults = `<button class="recipe" value=${data.results[i].id}> ${data.results[i].title}</button>`
+
+        console.log(recipeResults)
+        resultsContainer.append(recipeResults)
     }
 
-    $('.recipe').on('click', function(event){
+    $('.recipe').on('click', function(event) {
         var recipeId = event.target.value;
         console.log(recipeId)
         getRecipeApi(recipeId);
@@ -36,38 +35,39 @@ function displayResults (data) {
 
 
 
-function getRecipeApi(recipeId){
-    var recipe = "https://api.spoonacular.com/recipes/"+recipeId+"/information?&apiKey=7422e4770d3c4bdbb9679e356fa65ecf"
+function getRecipeApi(recipeId) {
+    var recipe = "https://api.spoonacular.com/recipes/" + recipeId + "/information?&apiKey=7422e4770d3c4bdbb9679e356fa65ecf"
     fetch(recipe)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
-  
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+
 }
 
-function getIngredientsApi(recipeId){
-    var ingredients = "https://api.spoonacular.com/recipes/"+recipeId+"/ingredientWidget.json?&apiKey=7422e4770d3c4bdbb9679e356fa65ecf"
+function getIngredientsApi(recipeId) {
+    var ingredients = "https://api.spoonacular.com/recipes/" + recipeId + "/ingredientWidget.json?&apiKey=7422e4770d3c4bdbb9679e356fa65ecf"
     fetch(ingredients)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        for (i = 0; i < data.length; i++ ){
-            var  recipeList = $("<div>")
-            recipeList.text(data[i])
-            $("#recipeIngredients").append(recipeList)
-            } 
-    })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            for (i = 0; i < data.ingredients.length; i++) {
+                var recipeList = $("<div>")
+                console.log(data.ingredients[i].name);
+                recipeList.text(data.ingredients[i].name)
+                $("#recipeIngredients").append(recipeList)
+            }
+        })
 
 }
 
 
-confirmButton.on("click", function(){
+confirmButton.on("click", function() {
     cuisineSelection = $(".form-control").val()
 
-getAPI(cuisineSelection);    
+    getAPI(cuisineSelection);
 })
