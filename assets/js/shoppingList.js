f7773b10c4fa34b9e90d2dc4e11ccf2a7fff69fa
kembo001 <47574348+kembo001@users.noreply.github.com>
2021-10-06 11:18:17 -0500

    changes to the js file

--- assets/js/shoppingList.js
+++ assets/js/shoppingList.js
@@ -1,7 +1,17 @@
 //  selectors 
-var numberOfItems = document.getElementById("numberOfItems").innerHTML;
-var nameOfItem = document.getElementById("nameOfItem").innerHTML;
-var setCart;
+// var numberOfItems = document.getElementById("numberOfItems").innerHTML;
+var nameOfItem = document.getElementById("nameOfItem");
+var saveOfItem = document.getElementById("nameOfItem");
+
+var shoppingList = document.getElementById("shoppingListContainer");
+
+// var nameArray = ["tea", "milk","beans"];
+var nameArray = []
+var setCart = [];
+var getIngredientsForCart = localStorage.getItem
+if(localStorage.getItem("cart")){
+    setCart = JSON.parse(localStorage.getItem("cart"))
+}
 var emptyListText = document.getElementById("emptyList")
 var removeButton = document.getElementById('removeItem');
 var saveItems = document.getElementById("saveItems");
@@ -11,9 +21,8 @@ cart.style.visibility = 'hidden';
 
 saveItems.addEventListener('click', function(){
     console.log('saved')
-    setCart = []
         var entry ={
-            "name": nameOfItem,
+            "name": saveOfItem,
             "Numberofitems":numberOfItems
         };
         setCart.push(entry);
@@ -22,28 +31,83 @@ saveItems.addEventListener('click', function(){
 });
 
 
-// global variables
-for (var i = 0; i < removeButton.length; i++){
-    var button = removeButton[i]
-    button.addEventListener('click', function() {
-        console.log("remove")
-        // list.remove()
-    })
+/*
+
+<li class="list-group-item" id="numberOfItems">1</li>
+            <li class="list-group-item" id="nameOfItem"></li>
+            <li class="list-group-item" id="removeItem"></li>
+
+*/
+
+
+
+// get from local storage
+var food = JSON.parse(localStorage.getItem("Ingredients"))
+console.log(food)
+nameArray = food 
+
+
+
+// --------------------------------
+// render Item function
+// --------------------------------
+function renderItems(){
+    for(i=0; i<nameArray.length; i++){
+        var row = document.createElement("div");
+        row.setAttribute("class", "row");
+
+        var col1 = document.createElement("div");
+        var col2 = document.createElement("div");
+        var col3 = document.createElement("div");
+
+        col1.setAttribute("class", "col-2 item-index");
+        col2.setAttribute("class", "col-8 item-name");
+        col3.setAttribute("class", "col-2 item-delete");
+
+        var newButton = document.createElement('button');
+        newButton.textContent = 'X';
+        newButton.setAttribute("data-item-idx", i);
+        newButton.addEventListener('click', function(){
+            var selectedItem = this.row;
+            nameArray.splice(selectedItem, 1)
+            console.log("hello")
+        })
+        col1.textContent = i;
+        col2.textContent = nameArray[i];
+        col3.appendChild(newButton);
+
+        row.appendChild(col1);
+        row.appendChild(col2);
+        row.appendChild(col3);
+
+        shoppingList.appendChild(row);
+    }
 }
 
-// functions
+
+// --------------------------------
+// shopping Cart function
+// --------------------------------
 function shoppingCart(){
     emptyListText.style.visibility = 'hidden';
     cart.style.visibility = 'visible';
+    renderItems()
 }
-shoppingCart()
 
 
+
+// -------------------------------
+// reset cart function
+// -------------------------------
 function resetCart(){
    
 }
 
+// create an event listener on the parenr element of all delete buttons
+// the button clicked on will have a data attribute for the index of the item being removed
+// delete that item from the array
 
 
+// execute Function
 
-
+shoppingCart()
